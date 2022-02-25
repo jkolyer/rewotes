4f12bd7e3c3246faf22a5b9a30a9fa75e8b6c650
Jonathan Kolyer <jonathankolyer@gmail>
2022-02-25 12:32:09 -0800

    pfu-10: sqlite connect/disconnect

--- jkolyer/jkolyer/orchestration.py
+++ jkolyer/jkolyer/orchestration.py
@@ -1,9 +1,32 @@
 import sqlite3
+import logging
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG)
 
 class Orchestration:
     def __init__(self):
         pass
 
+    def disconnectDb(self):
+        if self.sqliteConnection is None: return
+        self.sqliteConnection.close()
+        self.sqliteConnection = None
+        logger.info("The SQLite connection is closed")
     
+    def connectDb(self):
+        try:
+            self.sqliteConnection = sqlite3.connect('parallel-file-upload.db')
+            cursor = self.sqliteConnection.cursor()
+            logger.debug("Database created and Successfully Connected to SQLite")
 
+            sqlite_select_Query = "select sqlite_version();"
+            cursor.execute(sqlite_select_Query)
+            record = cursor.fetchall()
+            logger.debug(f"SQLite Database Version is: {record}")
+            cursor.close()
 
+        except sqlite3.Error as error:
+            self.sqliteConnection = None
+            logger.error(f"Error while connecting to sqlite: {error}")
+            
--- jkolyer/test/test_jkolyer.py
+++ jkolyer/test/test_jkolyer.py
@@ -12,17 +12,24 @@ class TestJkolyer(object):
     def setup_class(cls):
         pass
 
-    def test_something(self):
-        pass
-
     @classmethod
     def teardown_class(cls):
         pass
 
 
 class TestOrchestration(TestJkolyer):
-
     def test_create(self):
+        self.orch = Orchestration()
+        assert self.orch != None
+
+    def test_connectDb(self):
         orch = Orchestration()
-        assert orch != None
+        orch.connectDb()
+        assert orch.sqliteConnection is not None
+
+    def test_disconnectDb(self):
+        orch = Orchestration()
+        orch.connectDb()
+        orch.disconnectDb()
+        assert orch.sqliteConnection is None
 
